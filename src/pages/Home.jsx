a3c4e32da3c7b19d18dd09c311d6ffab8cd39e5f
alsharieff kallun <alsharieffk@gmail.com>
2026-08-17 02:03:54 +0800

    update part2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import Hero from "../components/sections/Hero";
 import Featured from "../components/sections/Featured";
 import Services from "../components/sections/Services";
-import Work from "../components/sections/Work";
-import Contact from "../components/sections/Contact";
 import FeaturedCarousel from "../components/sections/FeaturedWorkCarousel";
 import About from "../components/sections/About";
 import CTAButton from "../components/sections/CTA-Button";
+import Contact from "../components/sections/Contact";
 
 export default function Home() {
   return (
